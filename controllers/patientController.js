917589f67c591c3942350eb0f4bc2399875facd9
Saintcyr77 <shivam.bhatt04@gmail.com>
2024-01-17 20:21:36 +0530

    patients json

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,6 +1,26 @@
 
-const get_patients = (req,res) => {
-    res.json({he: "hello"})
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "hosdummy"
+})
+const get_patients = async (req, res) => {
+    try {
+        const data = await connection.promise().query(
+            `Select * from patients`
+        )
+        res.status(202).json({
+            patients: data[0]
+        })
+    }
+    catch (err) {
+        res.status(500).json({
+            message: err
+        })
+    }
+   
 }
 
 module.exports = {
